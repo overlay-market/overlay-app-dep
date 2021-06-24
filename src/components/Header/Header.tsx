54f06c37186af53402a5aa49ec4a33fb4677a327
Jonah Lin <57741810+linjonah@users.noreply.github.com>
2021-06-23 20:35:49 -0700

    chore: add logo to header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { useActiveWeb3React } from '../../hooks/web3';
 import { SupportedChainId } from '../../constants/chains';
 import { useDarkModeManager } from '../../state/user/hooks';
+import OverlayLogo from '../../assets/images/overlay-logo.png';
 import styles from './Header.module.scss';
 import styled from 'styled-components/macro';
 
@@ -19,6 +20,16 @@ export const HeaderContainer = styled.div`
   justify-content: space-between;
 `
 
+export const LogoContainer = styled.div`
+  height: 32px;
+  width: 32px;
+
+  img {
+    width: 100%;
+    height: 100%;
+  }
+`
+
 const NETWORK_LABELS: { [chainId in SupportedChainId | number]: string } = {
   [SupportedChainId.MAINNET]: 'Mainnet',
   [SupportedChainId.KOVAN]: 'Kovan',
@@ -31,7 +42,9 @@ export default function Header() {
 
   return (
     <HeaderContainer>
-      <div>Overlay</div>
+      <LogoContainer>
+        <img src={OverlayLogo} />
+      </LogoContainer>
       <div>Markets</div>
       <div>Positions</div>
 
