b0bbae24c56f4caa1bd22a0aeda45fe044113065
Jonah Lin <57741810+linjonah@users.noreply.github.com>
2021-06-23 22:07:46 -0700

    chore: remove unused import

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,12 +1,10 @@
 import './App.css';
-import React, { useState } from 'react';
+import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import Web3ReactManager from '../components/Web3ReactManager/Web3ReactManager';
 import Header from '../components/Header/Header';
 import Markets from './Markets/Markets';
 import Positions from './Positions/Positions';
-import { useActiveWeb3React } from '../hooks/web3';
-import { injected } from '../connectors/connectors';
 
 const App = () => {
   return (
