e26203bd08b095dbd8c6b4f3bdab845de7eccb6b
Jonah Lin <57741810+linjonah@users.noreply.github.com>
2021-06-29 19:21:40 -0700

    feat: add application updater

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,7 @@ import { Provider } from 'react-redux';
 import { HashRouter } from 'react-router-dom';
 import { createWeb3ReactRoot, Web3ReactProvider } from '@web3-react/core';
 import { NetworkContextName } from './constants/misc';
+import ApplicationUpdater from './state/application/updater';
 import ThemeProvider from "./theme/theme";
 import store from './state/state';
 import getLibrary from './utils/getLibrary'
@@ -13,12 +14,21 @@ import reportWebVitals from "./reportWebVitals";
 
 const Web3ProviderNetwork = createWeb3ReactRoot(NetworkContextName)
 
+function Updaters() {
+  return (
+    <>
+      <ApplicationUpdater />
+    </>
+  )
+}
+
 ReactDOM.render(
   <StrictMode>
     <Provider store={store}>
       <HashRouter>
         <Web3ReactProvider getLibrary={getLibrary}>
           <Web3ProviderNetwork getLibrary={getLibrary}>
+            <Updaters />
             <ThemeProvider>
               <App />
             </ThemeProvider>
