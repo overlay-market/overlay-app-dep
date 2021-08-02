2e65bf320256e6580d0747b89237568824ab8bbb
Jonah Lin <57741810+linjonah@users.noreply.github.com>
2021-08-02 14:21:19 -0700

    refactor: clean up header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,35 +1,13 @@
-import React from 'react';
+import { NavLink } from 'react-router-dom';
+import { Image } from 'rebass';
+import styled from 'styled-components/macro';
 import { useActiveWeb3React } from '../../hooks/web3';
 import { useDarkModeManager } from '../../state/user/hooks';
-import { NavLink  } from 'react-router-dom';
+import { useETHBalances } from '../../state/wallet/hooks';
+import { Row } from '../Row/Row';
 import Web3Status from '../Web3Status/Web3Status';
 import OverlayLogo from '../../assets/images/overlay-logo.png';
 import LightOverlayLogo from '../../assets/images/overlay-logo-light.png';
-import styles from './Header.module.scss';
-import styled from 'styled-components/macro';
-import { Row } from '../Row/Row';
-import { Moon, Sun } from 'react-feather';
-import { useETHBalances } from '../../state/wallet/hooks';
-
-export const StyledMenuButton = styled.button`
-  background-color: ${({theme}) => theme.bg3};
-  color: ${({theme}) => theme.text1};
-  border: none;
-  height: 35px;
-  border-radius: 0.5rem;
-  padding: 0.15rem 0.5rem;
-  display: flex;
-
-  :hover {
-    cursor: pointer;
-    outline: none;
-    background-color: ${({ theme }) => theme.bg3};
-  }
-
-  svg {
-    margin: auto;
-  }
-`
 
 export const HeaderContainer = styled.div`
   background-color: ${({theme}) => theme.bg1};
@@ -46,11 +24,6 @@ export const LogoContainer = styled.div`
   height: 32px;
   width: 32px;
   margin: auto 16px auto 0px;
-
-  img {
-    width: 100%;
-    height: 100%;
-  }
 `
 
 export const AccountContainer = styled(Row)`
@@ -81,9 +54,19 @@ export default function Header() {
     <HeaderContainer>
       <LogoContainer>
         {darkMode ? (
-          <img src={LightOverlayLogo} alt={'Overlay Logo Light'} />
+          <Image 
+            src={LightOverlayLogo} 
+            alt={'Overlay Logo Light'} 
+            height={'100%'}
+            width={'100%'}
+            />
           ) : (
-            <img src={OverlayLogo} alt={'Overlay Logo'} />
+          <Image 
+            src={OverlayLogo} 
+            alt={'Overlay Logo'} 
+            height={'100%'}
+            width={'100%'}
+            />
           )}
       </LogoContainer>
 
@@ -101,9 +84,6 @@ export default function Header() {
 
       <AccountContainer>
          <Web3Status/>
-        {/* <StyledMenuButton onClick={() => toggleDarkMode()}>
-          {darkMode ? <Moon size={20} /> : <Sun size={20} />}
-        </StyledMenuButton> */}
       </AccountContainer>
     </HeaderContainer>
   )
