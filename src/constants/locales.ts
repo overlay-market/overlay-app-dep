b2991ad6911c5274ead2ba6688e1d6d9e4f122cb
Jonah Lin <57741810+linjonah@users.noreply.github.com>
2021-08-05 13:50:37 -0700

    feat: add locales en fr vn cn

---
+++ src/constants/locales.ts
@@ -0,0 +1,17 @@
+export const SUPPORTED_LOCALES = [
+  // order as they appear in the language dropdown
+  'en-US',
+  'fr-FR',
+  'vi-VN',
+  'zh-CN',
+] as const
+export type SupportedLocale = typeof SUPPORTED_LOCALES[number]
+
+export const DEFAULT_LOCALE: SupportedLocale = 'en-US'
+
+export const LOCALE_LABEL: { [locale in SupportedLocale]: string } = {
+  'en-US': 'English',
+  'fr-FR': 'français',
+  'vi-VN': 'Tiếng Việt',
+  'zh-CN': '中文 ( 中国 )',
+}
