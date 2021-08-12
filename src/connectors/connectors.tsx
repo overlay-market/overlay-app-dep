071c6c6c0b652fca9289d95249de2617db8c4f2c
Jonah Lin <57741810+linjonah@users.noreply.github.com>
2021-08-12 16:10:01 -0700

    feat: add wallte constants - metamask + walletconnect

--- src/connectors/connectors.tsx
+++ src/connectors/connectors.tsx
@@ -4,8 +4,16 @@ import { SupportedChainId } from '../constants/chains';
 const SUPPORTED_CHAIN_IDS: SupportedChainId[] = [
   SupportedChainId.MAINNET,
   SupportedChainId.KOVAN
-]
+];
 
 export const injected = new InjectedConnector({
   supportedChainIds: SUPPORTED_CHAIN_IDS,
-})
+});
+
+// export const walletconnect = new WalletConnectConnector({
+//   supportedChainIds: SUPPORTED_CHAIN_IDS,
+//   rpc: NETWORK_URLS,
+//   bridge: WALLETCONNECT_BRIDGE_URL,
+//   qrcode: true,
+//   pollingInterval: 15000,
+// });
