4ce702bd2db987f7a9d313478c62f44f70ebb93c
Jonah Lin <57741810+linjonah@users.noreply.github.com>
2021-08-19 16:48:04 -0700

    feature: infocolumn component

---
+++ src/components/InfoColumn/InfoColumn.tsx
@@ -0,0 +1,71 @@
+import styled from 'styled-components';
+
+const Container = styled.div<{ align?: string, margin?: string}>`
+  display: flex;
+  flex-direction: column;
+  text-align: ${({align}) => (align ? align : 'left')};
+  margin: ${({margin}) => (margin ? margin : 0)};
+`;
+
+const Title = styled.div<{ 
+  titleColor?: string, 
+  titleFontSize?: string, 
+  titleFontWeight?: number
+}>`
+  color: ${({theme, titleColor}) => (titleColor ? titleColor : theme.text1)};
+  font-size: ${({titleFontSize}) => (titleFontSize ? titleFontSize : '14px')};
+  font-weight: ${({titleFontWeight}) => (titleFontWeight ? titleFontWeight : 400)};
+`;
+
+const Description = styled.div<{ 
+  descriptionColor?: string,
+  descriptionFontSize?: string,
+  descriptionFontWeight?: number,
+}>`
+  color: ${({theme, descriptionColor}) => (descriptionColor ? descriptionColor : theme.text1)};
+  font-size: ${({descriptionFontSize}) => (descriptionFontSize ? descriptionFontSize : '20px')};
+  font-weight: ${({descriptionFontWeight}) => (descriptionFontWeight ? descriptionFontWeight : 700)};
+`;
+
+export const InfoColumn = ({
+  title,
+  description,
+  margin,
+  align,
+  titleColor,
+  titleFontSize,
+  titleFontWeight,
+  descriptionColor,
+  descriptionFontSize,
+  descriptionFontWeight
+}:{
+  title: string
+  description: string
+  margin?: string
+  align?: string
+  titleColor?: string
+  titleFontSize?: string
+  titleFontWeight?: number
+  descriptionColor?: string
+  descriptionFontSize?: string
+  descriptionFontWeight?: number
+}) => {
+  return (
+    <Container align={align} margin={margin}>
+      <Title
+        titleColor={titleColor}
+        titleFontSize={titleFontSize}
+        titleFontWeight={titleFontWeight}
+        >
+          {title}
+      </Title>
+      <Description
+        descriptionColor={descriptionColor}
+        descriptionFontSize={descriptionFontSize}
+        descriptionFontWeight={descriptionFontWeight}
+        >
+          {description}
+      </Description>
+    </Container>
+  )
+};
--- src/pages/Magic/Magic.tsx
+++ src/pages/Magic/Magic.tsx
@@ -1,6 +1,12 @@
 import React, { useState } from 'react';
 import styled from 'styled-components';
 import { MarketCard } from '../../components/Card/MarketCard';
+import { InfoColumn } from '../../components/InfoColumn/InfoColumn';
+import { NumericalInput } from '../../components/NumericalInput/NumericalInput';
+import { Row } from '../../components/Row/Row';
+import { Label } from '@rebass/forms';
+import { TEXT } from '../../theme/theme';
+import { TransparentUnderlineButton } from '../../components/Button/Button';
 
 const Container = styled.div`
   display: flex;
@@ -9,18 +15,95 @@ const Container = styled.div`
   margin: 0 auto 16px;
 `;
 
+const MagicDetail = styled.div`
+  display: flex;
+  flex-direction: row;
+  width: 100%;
+  margin-bottom: 24px;
+`;
+
+const InputContainer = styled(Row)`
+  border-radius: 4px;
+  overflow: hidden;
+  border: 1px solid ${({theme}) => theme.white};
+  margin-bottom: 24px;
+`;
+
+const InputType = styled.div<{
+  height?: string
+  padding?: string
+  background?: string
+}>`
+  background: ${({background}) => (background ? background : '#E0E0E0')};
+  color: ${({theme}) => theme.text1};
+  height: ${({height}) => height};
+  line-height: ${({height}) => height};
+  padding: ${({padding}) => padding};
+  font-size: 14px;
+`;
+
 const Magic = ({
   children
 }:{
   children: React.ReactNode
 }) => {
+  const [amount, setAmount] = useState(undefined);
+
+  const handleAmountInput = (e: any) => {
+    setAmount(e.target.value);
+  };
+
   return (
     <Container>
       <MarketCard 
-        title={'Make some magic'}
+        title={'Make some Magic'}
         align={'center'}
         >
-
+        <Label htmlFor='Amount' mt={'24px'}>
+          <TEXT.Body margin={'0 auto 4px 0'} color={'white'}>
+            Amount
+          </TEXT.Body>
+          <Row 
+            ml={'auto'} 
+            mb={'4px'} 
+            width={'auto'}
+            >
+            <TransparentUnderlineButton>25%</TransparentUnderlineButton>
+            <TransparentUnderlineButton>50%</TransparentUnderlineButton>
+            <TransparentUnderlineButton>75%</TransparentUnderlineButton>
+            <TransparentUnderlineButton>Max</TransparentUnderlineButton>
+          </Row>
+        </Label>
+        <InputContainer>
+          <NumericalInput
+            value={amount}
+            onUserInput={handleAmountInput}
+            />
+          <InputType
+            background={'transparent'}
+            padding={'4px'}
+            >
+            ETH
+          </InputType>
+        </InputContainer>
+        <MagicDetail>
+          <InfoColumn
+            title={'magicETH'}
+            description={'$0.0'}
+            margin={'0 auto 0 0'}
+            />
+          <InfoColumn
+            title={'Funding rate:'}
+            description={'~ -0.0026%'}
+            descriptionColor={'#10DCB1'}
+            margin={'0 auto 0 0'}
+            />
+          <InfoColumn
+            title={'OVL earned'}
+            description={'0.0'}
+            margin={'0 auto 0 0'}
+            />
+        </MagicDetail>
       </MarketCard>
     </Container>
   )
