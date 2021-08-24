f36fa3c696deea2e3cfe1ee3f3203a56c77a69d3
Jonah Lin <57741810+linjonah@users.noreply.github.com>
2021-08-24 15:13:04 -0700

    style: breadcrumbs, bold current

--- src/components/Breadcrumbs/Breadcrumbs.tsx
+++ src/components/Breadcrumbs/Breadcrumbs.tsx
@@ -1,6 +1,22 @@
 import { NavLink } from 'react-router-dom';
+import styled from 'styled-components';
 import useBreadcrumbs from 'use-react-router-breadcrumbs';
 import routesConfig from '../../routes/routesConfig';
+import { ChevronRight } from 'react-feather';
+
+const Breadcrumb = styled.span`
+  color: ${({theme}) => theme.text1};
+`;
+
+const StyledNavLink = styled(NavLink)`
+  color: ${({theme}) => theme.text1};
+  text-decoration: none;
+  font-size: 12px;
+`;
+
+const ActiveNavLink = styled(StyledNavLink)`
+  font-weight: 700;
+`;
 
 export const Breadcrumbs = () => {
   const breadcrumbs = useBreadcrumbs(routesConfig);
@@ -10,10 +26,19 @@ export const Breadcrumbs = () => {
       {breadcrumbs.map(({
         match,
         breadcrumb
-      }) => (
-        <span key={match.url}>
-          <NavLink to={match.url}>{breadcrumb}</NavLink>
-        </span>
+      }, index) => (
+        <Breadcrumb key={match.url}>
+          {index === breadcrumbs.length - 1 ? (
+            <ActiveNavLink to={match.url}>
+              {breadcrumb}
+            </ActiveNavLink>
+          ):(
+            <StyledNavLink to={match.url}>
+                {breadcrumb}
+            </StyledNavLink>
+          )}
+          {index < breadcrumbs.length - 1 && <ChevronRight height={8} width={8} />}
+        </Breadcrumb>
       ))}
     </>
   );
