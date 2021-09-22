81c23a1e964eb4206cdf46875d6d77321c94320b
Jonah Lin <57741810+linjonah@users.noreply.github.com>
2021-09-21 17:14:22 -0700

    feat: progress bar transition starts from 0 to current always

--- src/components/ProgressBar/ProgressBar.tsx
+++ src/components/ProgressBar/ProgressBar.tsx
@@ -1,3 +1,4 @@
+import { useState, useEffect } from 'react';
 import styled from 'styled-components';
 
 const Container = styled.div<{ width: string; margin?: string }>`
@@ -9,6 +10,7 @@ const Container = styled.div<{ width: string; margin?: string }>`
 const ProgressBackground = styled.div`
   border-radius: 30px;
   border: 1px solid #F2F2F2;
+  overflow: hidden;
   background: rgba(0, 0, 0, 0.25);  
   box-shadow: inset 0 1px 2px rgba(0, 0, 0, 0.25), 0 1px rgba(255, 255, 255, 0.08);
 `;
@@ -18,11 +20,11 @@ const Bar = styled.div<{ width?: number; color: string }>`
   border-radius: 30px;
   background-image: 
     linear-gradient(to bottom, rgba(255, 255, 255, 0.3), rgba(255, 255, 255, 0.05));
-  transition: 0.4s linear;  
+  transition: 1s ease-out;  
   transition-property: width, background-color;  
   width: ${({ width }) => ( `${width}%` )};
   background-color: ${({ color }) => ( color )};  
-  animation: progressAnimation 2s;
+  animation: progressAnimation 1s;
 `;
 
 export const ProgressBar = ({
@@ -38,12 +40,21 @@ export const ProgressBar = ({
   width: string
   margin?: string | undefined
 }) => {
+  const [ progressValue, setProgressValue ] = useState(0);
+
   let currentPercentage = value ? ( (value / max) * 100 ) : 0;
+
+  useEffect(() => {
+    if (progressValue !== currentPercentage ) {
+      setProgressValue( currentPercentage );
+    }
+  }, [currentPercentage, progressValue])
+
 
   return (
     <Container width={width} margin={margin}>
       <ProgressBackground>
-        <Bar width={currentPercentage} color={color}/>
+        <Bar width={progressValue} color={color}/>
       </ProgressBackground>
     </Container>
   )
