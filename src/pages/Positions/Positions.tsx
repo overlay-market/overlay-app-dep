77fea1a3bc0969c7bbcc9b6cfcce97f5935f9ee9
Jonah Lin <57741810+linjonah@users.noreply.github.com>
2021-10-05 16:15:24 -0700

    feat: build PositionsChartHeader

--- src/pages/Positions/Positions.tsx
+++ src/pages/Positions/Positions.tsx
@@ -33,6 +33,25 @@ const Header = styled.div`
   color: white;
 `;
 
+const TableHeader = styled.div`
+  width: 100%;
+  border-bottom: 1px solid #828282;
+  padding-bottom: 8px;
+  color: white;
+  display: flex;
+  flex-direction: row;
+`;
+
+const HeaderCell = styled.div<{ 
+  align?: string,
+  width: string
+}>`
+  text-align: ${({ align }) => ( align ? align : 'left' )};
+  width: ${({ width }) => ( width ? width : 'auto' )};
+  font-size: 14px;
+  font-weight: 700;
+`
+
 const LoadingContainer = styled.div`
   display: block;
 `;
@@ -48,6 +67,23 @@ const ConnectWallet = styled(Button)`
   }
 `;
 
+const PositionsChartHeader = () => (
+  <TableHeader>
+    <HeaderCell align="left" width="50%">
+      Position
+    </HeaderCell>
+
+    <HeaderCell align="left" width="30%">
+      Est. Liq.
+    </HeaderCell>
+
+    <HeaderCell align="right" width="20%">
+      PnL
+    </HeaderCell>
+  </TableHeader>
+)
+
+
 const Positions = () => { 
   const { account, activate, chainId } = useActiveWeb3React();
   const [loading, setLoading] = useState(true);
@@ -59,6 +95,8 @@ const Positions = () => {
       <Header>
         Positions
       </Header>
+
+      <PositionsChartHeader />
       {account ? (
         <LoadingContainer>
           <PlanckCatLoader duration={5} width={24} />
