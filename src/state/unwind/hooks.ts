684b47328719dd6d4b37626e289c32d716501880
Jonah Lin <57741810+linjonah@users.noreply.github.com>
2021-12-01 16:31:21 -0800

    onSelectPositionId action + hook

--- src/state/unwind/hooks.ts
+++ src/state/unwind/hooks.ts
@@ -1,6 +1,32 @@
-import { useAppSelector } from "../hooks";
+import { useCallback } from "react";
+import { useAppSelector, useAppDispatch } from "../hooks";
 import { AppState } from "../state";
+import { typeInput, selectPositionId } from './actions';
 
 export function useUnwindState(): AppState['unwind'] {
   return useAppSelector((state) => state.unwind);
+};
+
+export function useUnwindActionHandlers(): {
+  onUserInput: (typedValue: string) => void;
+  onSelectPositionId: (positionId: string) => void;
+} {
+  const dispatch = useAppDispatch();
+
+  const onUserInput = useCallback(
+    (typedValue: string) => {
+      dispatch(typeInput({ typedValue }))
+    }, [dispatch]
+  );
+
+  const onSelectPositionId = useCallback(
+    (positionId: string) => {
+      dispatch(selectPositionId({ positionId }))
+    }, [dispatch]
+  );
+
+  return {
+    onUserInput,
+    onSelectPositionId
+  }
 };
