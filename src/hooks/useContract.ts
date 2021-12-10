65345de16757fa1cfdb6a3c98d5e5a4227bb38b6
Jonah Lin <57741810+linjonah@users.noreply.github.com>
2021-12-10 12:33:59 -0800

    useCollateralManagerContract

--- src/hooks/useContract.ts
+++ src/hooks/useContract.ts
@@ -2,14 +2,14 @@ import { useMemo } from "react";
 import { useActiveWeb3React } from "./web3";
 import { Contract } from "@ethersproject/contracts";
 import { getContract } from "../utils/contract";
-import { MULTICALL2_ADDRESS } from "../constants/addresses";
-import { V1_FACTORY_ADDRESS } from "../constants/addresses";
+import { MULTICALL2_ADDRESS, V1_FACTORY_ADDRESS, OVL_COLLATERAL_ADDRESS } from "../constants/addresses";
 import OVL_V1_FACTORY_ABI from "../constants/abis/OVL_V1_Factory.json";
 import OVL_V1_MARKET_ABI from "../constants/abis/overlayv1_uniswapv3_market.json"
 import MULTICALL2_ABI from '../constants/multicall/multicall2.json';
 import { Erc20 as ERC20 } from "../constants/abis/types";
 import ERC20_ABI from '../constants/abis/erc20.json';
 import ERC20_BYTES32_ABI from '../constants/abis/erc20_bytes32.json';
+import OVL_COLLATERAL_ABI from '../constants/abis/OverlayV1OVLCollateral.json';
 
 // returns null on errors
 export function useContract<T extends Contract = Contract>(
@@ -47,6 +47,11 @@ export function useOVLFactoryContract(): Contract | null {
 
 export function useMarketContract(address: string | undefined): Contract | null {
   return useContract(address, OVL_V1_MARKET_ABI, false);
+};
+
+export function useCollateralManangerContract(): Contract | null {
+  const { chainId } = useActiveWeb3React();
+  return useContract(chainId && OVL_COLLATERAL_ADDRESS[chainId], OVL_COLLATERAL_ABI, false)
 };
 
 export function useTokenContract(tokenAddress?: string, withSignerIfPossible?: boolean): Contract | null {
