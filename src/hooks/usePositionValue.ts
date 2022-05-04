59c10451cb80d6590f826a9c3248ee1cf6f1c3d6
Jonah Lin <57741810+linjonah@users.noreply.github.com>
2022-05-04 10:20:09 -0700

    usePositionValue

---
+++ src/hooks/usePositionValue.ts
@@ -0,0 +1,34 @@
+import { useEffect, useState, useMemo } from "react";
+import { useV1PeripheryContract } from "./useContract";
+import { BigNumber } from "ethers";
+import { useBlockNumber } from "../state/application/hooks";
+import { useActiveWeb3React } from "./web3";
+
+export function usePositionValue(
+  marketAddress?: string,
+  positionId?: string | number
+): BigNumber | undefined {
+  const peripheryContract = useV1PeripheryContract();
+  const blockNumber = useBlockNumber();
+  const { account } = useActiveWeb3React();
+  const [value, setValue] = useState<BigNumber>();
+
+  useEffect(() => {
+    if (!peripheryContract || !marketAddress || !account || !blockNumber) return;
+
+    (async () => {
+      try {
+        setValue(await peripheryContract.value(marketAddress, account, positionId))
+      }
+      catch (error) {
+        console.log('market inside usePositionValue: ', marketAddress);
+        console.error('error coming from usePositionValue: ', error);
+      }
+
+    })();
+  }, [peripheryContract, marketAddress, positionId, blockNumber, account]);
+
+  return useMemo(() => {
+    return value;
+  }, [value]);
+};
