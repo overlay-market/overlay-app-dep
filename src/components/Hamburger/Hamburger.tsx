191ec42cdbda56de8e8b9a2900a31f713c9cd82a
Jonah Lin <57741810+linjonah@users.noreply.github.com>
2022-05-16 18:17:55 -0700

    mobile hamburger width

--- src/components/Hamburger/Hamburger.tsx
+++ src/components/Hamburger/Hamburger.tsx
@@ -3,6 +3,7 @@ import styled from 'styled-components';
 import { bool, func } from 'prop-types';
 
 export const StyledBurger = styled.button<{open: boolean}>`
+  min-width: 21px;
   position: block;
   top: 5%;
   right: 2rem;
