8e95e976e3d177183b548e6b30ea9271c8be3da3
Jonah Lin <57741810+linjonah@users.noreply.github.com>
2022-06-06 17:11:46 -0700

    add table to stake

--- src/pages/Stake/Stake.tsx
+++ src/pages/Stake/Stake.tsx
@@ -1,10 +1,35 @@
 import styled from "styled-components"
+import { PageContainer } from "../../components/Container/Container";
+import { TableContainer, TableHead, Paper } from '@material-ui/core';
+import { StyledTable, StyledHeaderCell, StyledTableCellThin, StyledTableRow, StyledTableHeaderRow } from '../../components/Table/Table';
 
 export const Stake = () => {
   return (
-    <>
-      Stake
-    </>
+    <PageContainer maxWidth={'690px'}>
+      <TableContainer component={Paper}>
+        <StyledTable>
+          <TableHead>
+            <StyledTableHeaderRow>
+              <StyledHeaderCell width={20}>
+                 Pool
+              </StyledHeaderCell>
+              <StyledHeaderCell width={20}>
+                Balance
+              </StyledHeaderCell>
+              <StyledHeaderCell width={20}>
+                TVL
+              </StyledHeaderCell>
+              <StyledHeaderCell width={20}>
+                Share %
+              </StyledHeaderCell>
+              <StyledHeaderCell width={20}>
+                APY
+              </StyledHeaderCell>
+            </StyledTableHeaderRow>
+          </TableHead>
+        </StyledTable>
+      </TableContainer>
+    </PageContainer>
   )
 };
 
