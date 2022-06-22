bc120d54619d0cbd7735efa1a9a976e37291ab6f
Jonah Lin <57741810+linjonah@users.noreply.github.com>
2022-06-22 16:37:08 -0700

    stake reducer

---
+++ src/state/stake/reducer.ts
@@ -0,0 +1,31 @@
+
+import { createReducer } from "@reduxjs/toolkit";
+import {
+  typeInput,
+  selectAction,
+  resetStakeState
+} from "./actions";
+
+export interface StakeState {
+  readonly typedValue: string | undefined;
+  readonly isStake: boolean | undefined;
+};
+
+export const initialState: StakeState = {
+  typedValue: "",
+  isStake: true
+};
+
+export default createReducer<StakeState>(initialState, (builder) =>
+  builder
+    .addCase(typeInput, (state, { payload: { typedValue } }) => {
+      state.typedValue = typedValue;
+    })
+    .addCase(selectAction, (state, { payload: { isStake }}) => {
+      state.isStake = isStake;
+    })
+    .addCase(resetStakeState, (state) => {
+      state.typedValue = initialState.typedValue;
+      state.isStake = initialState.isStake;
+    })
+);
