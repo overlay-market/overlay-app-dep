3459cb6c430b15cd434b77b035ad4710bff3c786
Jonah Lin <57741810+linjonah@users.noreply.github.com>
2022-08-17 14:46:41 -0700

    add TERMS_OF_SERVICE ApplicationModal type

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,18 +1,18 @@
-import './App.css';
-import { Route, Switch, Redirect } from 'react-router-dom';
-import Web3ReactManager from '../components/Web3ReactManager/Web3ReactManager';
-import Header from '../components/Header/Header';
-import CurrentBlock from '../components/CurrentBlock/CurrentBlock';
-import Markets from './Markets/Markets';
-import { Market } from './Markets/Market';
-import { Unwind } from './Positions/Unwind';
-import Positions from './Positions/Positions';
-import Liquidate from './Liquidate/Liquidate';
-import styled from 'styled-components/macro';
-import Magic from './Magic/Magic';
-import Vaults from './Stake/Vaults';
-import { Stake } from './Stake/Stake';
-import Popups from '../components/Popup/Popups';
+import './App.css'
+import {Route, Switch, Redirect} from 'react-router-dom'
+import Web3ReactManager from '../components/Web3ReactManager/Web3ReactManager'
+import Header from '../components/Header/Header'
+import CurrentBlock from '../components/CurrentBlock/CurrentBlock'
+import Markets from './Markets/Markets'
+import {Market} from './Markets/Market'
+import {Unwind} from './Positions/Unwind'
+import Positions from './Positions/Positions'
+import Liquidate from './Liquidate/Liquidate'
+import styled from 'styled-components/macro'
+import Magic from './Magic/Magic'
+import Vaults from './Stake/Vaults'
+import {Stake} from './Stake/Stake'
+import Popups from '../components/Popup/Popups'
 
 export const AppWrapper = styled.div`
   background-color: ${({theme}) => theme.bg1};
@@ -21,26 +21,43 @@ export const AppWrapper = styled.div`
   width: 100vw;
 `
 
+const TermsOfServiceManager = ({children}: {children: JSX.Element | JSX.Element[]}) => {
+  const isConsented = true
+
+  if (isConsented) {
+    return <>{children}</>
+  } else {
+    return <>'Will Not Show'</>
+  }
+}
+
 const App = () => {
   return (
     <AppWrapper>
       <Popups />
-      <Header />
-      <Web3ReactManager>
-        <Switch>
-          <Route exact strict path="/" render={() => <Redirect to="/markets" />} />
-          <Route exact strict path="/markets" component={Markets} />
-          <Route exact strict path="/markets/:marketId" component={Market} />
-          <Route exact strict path="/positions" component={Positions} />
-          <Route exact strict path="/positions/:marketPositionId/:positionId" component={Unwind} /> 
-          <Route exact strict path="/magic" component={Magic} />
-          <Route exact strict path="/liquidate" component={Liquidate} />
-          <Route exact strict path="/stake" component={Vaults} />
-          <Route exact strict path="/stake/:vaultId" component={Stake} />
-        </Switch>
-      </Web3ReactManager>
+      <TermsOfServiceManager>
+        <Header />
+        <Web3ReactManager>
+          <Switch>
+            <Route exact strict path="/" render={() => <Redirect to="/markets" />} />
+            <Route exact strict path="/markets" component={Markets} />
+            <Route exact strict path="/markets/:marketId" component={Market} />
+            <Route exact strict path="/positions" component={Positions} />
+            <Route
+              exact
+              strict
+              path="/positions/:marketPositionId/:positionId"
+              component={Unwind}
+            />
+            <Route exact strict path="/magic" component={Magic} />
+            <Route exact strict path="/liquidate" component={Liquidate} />
+            <Route exact strict path="/stake" component={Vaults} />
+            <Route exact strict path="/stake/:vaultId" component={Stake} />
+          </Switch>
+        </Web3ReactManager>
+      </TermsOfServiceManager>
       <CurrentBlock />
     </AppWrapper>
   )
 }
-export default App;
+export default App
--- src/state/application/actions.ts
+++ src/state/application/actions.ts
@@ -1,5 +1,5 @@
-import { createAction } from '@reduxjs/toolkit';
-import { TokenList } from '@uniswap/token-lists';
+import {createAction} from '@reduxjs/toolkit'
+import {TokenList} from '@uniswap/token-lists'
 
 export type PopupContent =
   | {
@@ -17,19 +17,21 @@ export type PopupContent =
         newList: TokenList
         auto: boolean
       }
-    };
+    }
 
 export enum ApplicationModal {
   WALLET,
-};
+  TERMS_OF_SERVICE,
+}
 
-export const updateChainId = createAction<{ chainId: number | null }>('application/updateChainId')
-export const updateBlockNumber = createAction<{ chainId: number; blockNumber: number }>('application/updateBlockNumber');
+export const updateChainId = createAction<{chainId: number | null}>('application/updateChainId')
+export const updateBlockNumber = createAction<{chainId: number; blockNumber: number}>(
+  'application/updateBlockNumber',
+)
 export const addPopup = createAction<{
   key?: string
   removeAfterMs?: number | null
   content: PopupContent
-}>('application/addPopup');
-export const removePopup = createAction<{ key: string }>('application/removePopup');
-export const setOpenModal = createAction<ApplicationModal | null>('application/setOpenModal');
-
+}>('application/addPopup')
+export const removePopup = createAction<{key: string}>('application/removePopup')
+export const setOpenModal = createAction<ApplicationModal | null>('application/setOpenModal')
