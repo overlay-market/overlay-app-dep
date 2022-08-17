39c2c9a7f61663d54258810e6819dd2f1adc2f41
Jonah Lin <57741810+linjonah@users.noreply.github.com>
2022-08-17 15:35:05 -0700

    add text and buttons to TermsOfServiceModal

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,5 +1,5 @@
-import styled from 'styled-components/macro';
-import { Button as RebassButton, ButtonProps as ButtonPropsOriginal } from 'rebass/styled-components';
+import styled from 'styled-components/macro'
+import {Button as RebassButton, ButtonProps as ButtonPropsOriginal} from 'rebass/styled-components'
 
 type ButtonProps = Omit<ButtonPropsOriginal, 'css'>
 
@@ -14,90 +14,96 @@ const BaseTemplateButton = styled(RebassButton)<
     color?: any
   } & ButtonProps
 >`
-  width: ${({ width }) => (width ? width : '100%')};
-  color: ${({ color }) => ( color ? color : '#f2f2f2' )};
-  padding: ${({ padding }) => (padding ? padding : '16px')};
-  border: ${({ border }) => ( border ?? border )};
+  width: ${({width}) => (width ? width : '100%')};
+  color: ${({color}) => (color ? color : '#f2f2f2')};
+  padding: ${({padding}) => (padding ? padding : '16px')};
+  border: ${({border}) => border ?? border};
   cursor: pointer;
   font-weight: 700;
   text-align: center;
   transition: transform 450ms ease;
   -moz-osx-font-smoothing: grayscale;
   -webkit-font-smoothing: antialiased;
-  font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Oxygen",
-  "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue",
-  sans-serif;
+  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen', 'Ubuntu',
+    'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue', sans-serif;
 `
 
 // border: ${({ active }) => ( active ? 'none !important' : '' )};
 export const SelectActionButton = styled(BaseTemplateButton)`
-  color: ${({ isDisabled }) => ( isDisabled ? '#63656D !important' : '')};
-  background: ${({ isDisabled }) => ( isDisabled ? '#D0D0D2 !important' : '')};
+  color: ${({isDisabled}) => (isDisabled ? '#63656D !important' : '')};
+  background: ${({isDisabled}) => (isDisabled ? '#D0D0D2 !important' : '')};
   height: auto;
   padding: 16px;
   border-radius: 8px;
-`;
+`
 
 export const TriggerActionButton = styled(SelectActionButton)`
-  color: ${({ theme, active }) => ( active ? theme.white : '#71d2ff' )};
-  background: ${({ active }) => ( active ? '#12B4FF' : 'transparent' )};
+  color: ${({theme, active}) => (active ? theme.white : '#71d2ff')};
+  background: ${({active}) => (active ? '#12B4FF' : 'transparent')};
   border: 1px solid transparent;
-  
+
   :hover {
-    border: 1px solid ${({ isDisabled }) => ( isDisabled ? '#FF648A' : '#71d2ff')};
-    box-shadow: ${({ isDisabled }) => ( isDisabled ? '0 0px 7px #FF648A' : '0 0px 5px')};
+    border: 1px solid ${({isDisabled}) => (isDisabled ? '#FF648A' : '#71d2ff')};
+    box-shadow: ${({isDisabled}) => (isDisabled ? '0 0px 7px #FF648A' : '0 0px 5px')};
   }
-`;
+`
 
 export const PendingActionButton = styled(SelectActionButton)`
-  color: #0B0F1C;
+  color: #0b0f1c;
   border: none;
-  background: linear-gradient(91.32deg, #10DCB1 0%, #33E0EB 26.86%, #12B4FF 52.65%, #5295F9 77.89%, #9874FF 102.61%);
-`;
+  background: linear-gradient(
+    91.32deg,
+    #10dcb1 0%,
+    #33e0eb 26.86%,
+    #12b4ff 52.65%,
+    #5295f9 77.89%,
+    #9874ff 102.61%
+  );
+`
 
 export const ApproveTransactionButton = ({
   attemptingTransaction,
-  onClick
-}:{
-  attemptingTransaction: boolean;
-  onClick: () => void;
+  onClick,
+}: {
+  attemptingTransaction: boolean
+  onClick: () => void
 }) => (
   <>
     {attemptingTransaction ? (
-      <PendingActionButton>
-        Pending Confirmation...
-      </PendingActionButton>
-    ):(
-      <PendingActionButton onClick={onClick}>
-        Approve
-      </PendingActionButton>
+      <PendingActionButton>Pending Confirmation...</PendingActionButton>
+    ) : (
+      <PendingActionButton onClick={onClick}>Approve</PendingActionButton>
     )}
   </>
-);
+)
 
 export const TransactionSettingsButton = styled(BaseTemplateButton)`
-  background: ${({ active, isDisabled }) => ( active ? '#12B4FF' : isDisabled ? 'gray' : 'transparent')};
-  color: ${({ isDisabled }) => ( isDisabled ? '#0B0F1C' : 'white' )};
-  cursor: ${({ isDisabled }) => ( isDisabled ? 'none' : 'cursor' )};
+  background: ${({active, isDisabled}) =>
+    active ? '#12B4FF' : isDisabled ? 'gray' : 'transparent'};
+  color: ${({isDisabled}) => (isDisabled ? '#0B0F1C' : 'white')};
+  cursor: ${({isDisabled}) => (isDisabled ? 'none' : 'cursor')};
   height: 40px;
-`;
+`
 
-export const TransparentButton = styled(BaseTemplateButton)<{ width?: string, underline?: boolean }>`
+export const TransparentButton = styled(BaseTemplateButton)<{width?: string; underline?: boolean}>`
   width: ${({width}) => (width ? width : 'auto')};
-  color: ${({ color }) => ( color ? color : 'white' )};
+  color: ${({color}) => (color ? color : 'white')};
   padding: 0 8px;
   cursor: pointer;
   font-size: 12px;
   background: transparent;
-`;
+`
 
 export const TransparentUnderlineButton = styled(TransparentButton)`
   text-decoration: underline;
   border: none;
-`;
+`
 
 export const MenuButton = styled(BaseTemplateButton)`
   background: transparent;
   display: flex;
   font-size: 12px;
-`;
+`
+
+// @TO-DO: Refactor Green/Red Buttons from TermsOfServiceModal
+export const SolidColorButton = styled(BaseTemplateButton)``
--- src/components/TermsOfServiceModal/TermsOfServiceModal.tsx
+++ src/components/TermsOfServiceModal/TermsOfServiceModal.tsx
@@ -2,13 +2,25 @@ import {useState, useEffect} from 'react'
 import styled from 'styled-components'
 import {useModalOpen, useTermsOfServiceModalToggle} from '../../state/application/hooks'
 import {ApplicationModal} from '../../state/application/actions'
+import {SolidColorButton} from '../Button/Button'
+import {TEXT} from '../../theme/theme'
+import {FlexRow} from '../Container/Container'
 import Modal from '../Modal/Modal'
 
-export const ModalContent = styled.div`
+const ModalContent = styled.div`
   display: flex;
   flex-direction: column;
   padding: 16px;
   width: 100%;
+  background: rgb(46, 51, 72);
+`
+
+const UserAcceptButton = styled(SolidColorButton)`
+  background: green;
+`
+
+const UserRejectButton = styled(SolidColorButton)`
+  background: red;
 `
 
 export default function TermsOfServiceModal() {
@@ -24,7 +36,16 @@ export default function TermsOfServiceModal() {
       minHeight={false}
       maxHeight={90}
     >
-      <ModalContent>Lorem Ipsum</ModalContent>
+      <ModalContent>
+        <TEXT.BoldSmallBody lineHeight={1.5} m={'auto'}>
+          Please carefully read through the Terms of Service Agreement. By clicking "Accept", the
+          user is acknowledging to abide by the Terms of Service.
+        </TEXT.BoldSmallBody>
+        <FlexRow>
+          <UserAcceptButton>Accept</UserAcceptButton>
+          <UserRejectButton>Reject</UserRejectButton>
+        </FlexRow>
+      </ModalContent>
     </Modal>
   )
 }
