2c9f12cd456302cf426d3705ab7c0ec98a6b1104
Jonah Lin <57741810+linjonah@users.noreply.github.com>
2022-08-24 15:15:23 -0700

    init ChainalysisManager in App, move to own folder later

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -15,6 +15,10 @@ import Vaults from './Stake/Vaults'
 import {Stake} from './Stake/Stake'
 import Popups from '../components/Popup/Popups'
 
+import {useEffect} from 'react'
+import {useCookies} from 'react-cookie'
+import {useActiveWeb3React} from '../hooks/web3'
+
 export const AppWrapper = styled.div`
   background-color: ${({theme}) => theme.bg1};
   height: 100%;
@@ -22,31 +26,50 @@ export const AppWrapper = styled.div`
   width: 100vw;
 `
 
+function ChainalysisManager({children}: {children: JSX.Element | JSX.Element[]}) {
+  const {account} = useActiveWeb3React()
+  const [cookies] = useCookies()
+
+  // @TO-DO: check cookie on app initializing for any prior risk assessments
+
+  // @TO-DO: if cookie undefined, "GET" request chainanalysis API to check if address is registered
+  // if response message property value is 'Entity not found. Please be sure to register the Entity',
+  // we must register the account using a "POST" request to chainanalysis API.
+  useEffect(() => {
+    if (account) {
+      console.log('account in ChainalysisManager: ', account)
+    }
+  }, [account])
+  return <>{children}</>
+}
+
 const App = () => {
   return (
     <AppWrapper>
       <Popups />
-      <TermsOfServiceManager>
-        <Header />
-        <Web3ReactManager>
-          <Switch>
-            <Route exact strict path="/" render={() => <Redirect to="/markets" />} />
-            <Route exact strict path="/markets" component={Markets} />
-            <Route exact strict path="/markets/:marketId" component={Market} />
-            <Route exact strict path="/positions" component={Positions} />
-            <Route
-              exact
-              strict
-              path="/positions/:marketPositionId/:positionId"
-              component={Unwind}
-            />
-            <Route exact strict path="/magic" component={Magic} />
-            <Route exact strict path="/liquidate" component={Liquidate} />
-            <Route exact strict path="/stake" component={Vaults} />
-            <Route exact strict path="/stake/:vaultId" component={Stake} />
-          </Switch>
-        </Web3ReactManager>
-      </TermsOfServiceManager>
+      <ChainalysisManager>
+        <TermsOfServiceManager>
+          <Header />
+          <Web3ReactManager>
+            <Switch>
+              <Route exact strict path="/" render={() => <Redirect to="/markets" />} />
+              <Route exact strict path="/markets" component={Markets} />
+              <Route exact strict path="/markets/:marketId" component={Market} />
+              <Route exact strict path="/positions" component={Positions} />
+              <Route
+                exact
+                strict
+                path="/positions/:marketPositionId/:positionId"
+                component={Unwind}
+              />
+              <Route exact strict path="/magic" component={Magic} />
+              <Route exact strict path="/liquidate" component={Liquidate} />
+              <Route exact strict path="/stake" component={Vaults} />
+              <Route exact strict path="/stake/:vaultId" component={Stake} />
+            </Switch>
+          </Web3ReactManager>
+        </TermsOfServiceManager>
+      </ChainalysisManager>
       <CurrentBlock />
     </AppWrapper>
   )
