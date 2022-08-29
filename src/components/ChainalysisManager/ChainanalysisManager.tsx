3dd82679ffc6601e81d3781f91d9ee77a60d92e8
Jonah Lin <57741810+linjonah@users.noreply.github.com>
2022-08-29 15:36:36 -0700

    init useAxios hook executeGetAddress, used to hit /api/user/[walletAddress] after successful address registration

--- src/components/ChainalysisManager/ChainanalysisManager.tsx
+++ src/components/ChainalysisManager/ChainanalysisManager.tsx
@@ -14,39 +14,56 @@ export enum SecurityRiskLevels {
 
 enum LambdaEndpoint {
   REGISTER_ADDRESS = '/api/register/',
-  SCREEN_WALLET = '/api/user/',
+  SCREEN_ADDRESS = '/api/user/',
 }
 
-const apiUrl = '/api/register/'
 const mockSevereWalletAddress = '0x8576acc5c05d6ce88f4e49bf65bdf0c62f91353c'
 
 export default function ChainalysisManager({children}: {children: JSX.Element | JSX.Element[]}) {
   const {account} = useActiveWeb3React()
   const [cookies, setCookie] = useCookies([ClientCookies.userRiskLevel])
-
+  const [connectedAccount, setConnectedAccount] = useState('')
   const {userRiskLevel} = cookies
 
   // @TO-DO: use axios-hooks to manually trigger "GET" / "POST" requests to not perform unnecessary API calls
   // https://www.npmjs.com/package/axios-hooks
   const [
     {data: getRegisterData, loading: getRegisterLoading, error: getRegisterError},
-    executeQuery,
+    executeRegisterAddress,
   ] = useAxios(
     {
-      url: apiUrl + mockSevereWalletAddress,
+      url: LambdaEndpoint.REGISTER_ADDRESS + account,
       method: 'POST',
     },
     {manual: true},
   )
 
+  const [
+    {data: getAddressData, loading: getAddressLoading, error: getAddressError},
+    executeGetAddress,
+  ] = useAxios(
+    {
+      url: LambdaEndpoint.SCREEN_ADDRESS + account,
+      method: 'GET',
+    },
+    {manual: true},
+  )
+
   useEffect(() => {
-    executeQuery()
-  }, [])
+    if (account) {
+      executeRegisterAddress()
+        .then(response => {
+          console.log('executeRegisterAddress.then response: ', response)
+          executeGetAddress()
+        })
+        .catch(error => console.log('executeRegisterAddress.catch error: ', error))
+    }
+  }, [account])
 
   useEffect(() => {
     console.log('getRegisterData: ', getRegisterData)
-    console.log('getRegisterLoading: ', getRegisterLoading)
-  }, [getRegisterData, getRegisterLoading])
+    console.log('getAddressData: ', getAddressData)
+  }, [getRegisterData, getAddressData])
 
   // @TO-DO: useEffect to perform side effects on Chainanalysis "GET" request responses
   // if address has not been registered, perform "POST" request
