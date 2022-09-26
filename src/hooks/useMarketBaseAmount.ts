ff6ff5978027fe7c0425a1d755325e32ecad7842
Jonah Lin <57741810+linjonah@users.noreply.github.com>
2022-09-26 15:27:50 -0700

    init useMarketBaseAmount()

---
+++ src/hooks/useMarketBaseAmount.ts
@@ -0,0 +1,8 @@
+import {useEffect, useState, useMemo} from 'react'
+import {Interface} from '@ethersproject/abi'
+import {useSingleCallResult, useMultipleContractSingleData} from '../state/multicall/hooks'
+import {useUniswapV3Feed, useTokenContract} from './useContract'
+import UNISWAP_V3_FEED_ABI from '../constants/abis/OverlayV1UniswapV3Feed.json'
+import ERC20_INTERFACE from '../constants/abis/erc20'
+
+export function useMarketBaseAmounts(feedAddresses?: string) {}
