8988b4c5159a244ef42db36ed2caec046301eaa3
Jonah Lin <57741810+linjonah@users.noreply.github.com>
2022-11-15 16:08:06 -0800

    InfoTipProps

--- src/components/InfoTip/InfoTip.tsx
+++ src/components/InfoTip/InfoTip.tsx
@@ -1,71 +1,61 @@
-import ReactTooltip from "react-tooltip";
-import styled from 'styled-components/macro';
-import { isMobile } from "react-device-detect";
-import { Info } from "react-feather";
+import ReactTooltip from 'react-tooltip'
+import styled from 'styled-components/macro'
+import {isMobile} from 'react-device-detect'
+import {Info} from 'react-feather'
 
-const IconWrapper = styled.a<{ margin?: string }>`
+const IconWrapper = styled.a<{margin?: string}>`
   display: inline-flex !important;
-  margin: ${({margin}) => ( margin ? margin : 'auto 4px')};
+  margin: ${({margin}) => (margin ? margin : 'auto 4px')};
   vertical-align: middle;
-`;;
+`
 
-export const InfoTip = ({
-  children,
-  margin,
-  color,
-  tipFor,
-}:{
+type InfoTipProps = {
   children: React.ReactNode
   tipFor: string
   margin?: string
   color?: string
-}) => {
-  const mobileDataTip = tipFor + 'mobile';
-  const desktopDataTip = tipFor + 'desktop';
+}
+
+export const InfoTip = ({children, margin, color, tipFor}: InfoTipProps) => {
+  const mobileDataTip = tipFor + 'mobile'
+  const desktopDataTip = tipFor + 'desktop'
 
   return (
     <>
-      { isMobile ? (
+      {isMobile ? (
         <>
-          <IconWrapper 
-              data-for={mobileDataTip}
-              data-tip={mobileDataTip}
-              data-event="click focus"
-              >
-              <Info height={12} width={12}/>
+          <IconWrapper data-for={mobileDataTip} data-tip={mobileDataTip} data-event="click focus">
+            <Info height={12} width={12} />
           </IconWrapper>
 
-          <ReactTooltip 
-              place="top" 
-              type="dark" 
-              effect="solid"
-              textColor={color}
-              id={mobileDataTip}
-              globalEventOff="click"
-              >
-              { children }
+          <ReactTooltip
+            place="top"
+            type="dark"
+            effect="solid"
+            textColor={color}
+            id={mobileDataTip}
+            globalEventOff="click"
+          >
+            {children}
           </ReactTooltip>
         </>
-      ):(
+      ) : (
         <>
-          <IconWrapper 
-              data-for={desktopDataTip}
-              data-tip={desktopDataTip}
-              >
-              <Info height={12} width={12}/>
+          <IconWrapper data-for={desktopDataTip} data-tip={desktopDataTip}>
+            <Info height={12} width={12} />
           </IconWrapper>
 
-          <ReactTooltip 
-              place="bottom" 
-              type="dark" 
-              effect="solid"
-              textColor={color}
-              id={desktopDataTip}
-              >
-                { children }
+          <ReactTooltip
+            place="bottom"
+            type="dark"
+            effect="solid"
+            textColor={color}
+            id={desktopDataTip}
+          >
+            {children}
           </ReactTooltip>
         </>
       )}
     </>
   )
-};
+}
