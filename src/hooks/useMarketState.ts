e72ab881786836069d6d62504d24cef20b3531fa
Jonah Lin <57741810+linjonah@users.noreply.github.com>
2022-11-17 09:34:57 -0800

    init useMarketState

---
+++ src/hooks/useMarketState.ts
@@ -0,0 +1,5 @@
+import {useV1PeripheryContract} from './useContract'
+import {useSingleContractMultipleData} from '../state/multicall/hooks'
+import {useBlockNumber} from '../state/application/hooks'
+
+function useMarketStateFromAddresses() {}
