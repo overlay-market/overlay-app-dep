1ba7bba2bf984a5b318bf83c388e00ae0355d031
Jonah Lin <57741810+linjonah@users.noreply.github.com>
2022-11-17 13:06:03 -0800

    useMarketStateFromAddresses hook to return loading, error, and markets[]

--- src/hooks/useMarketState.ts
+++ src/hooks/useMarketState.ts
@@ -1,3 +1,5 @@
+import {CallState, Result} from './../state/multicall/hooks'
+import {useMemo, useEffect} from 'react'
 import {BigNumber} from 'ethers'
 import {useV1PeripheryContract} from './useContract'
 import {useSingleContractMultipleData} from '../state/multicall/hooks'
@@ -21,4 +23,44 @@ interface UseMarketStateResults {
   markets: MarketStateDetails[] | undefined
 }
 
-function useMarketStateFromAddresses() {}
+function useMarketStateFromAddresses(marketAddresses: string[] | undefined) {
+  const peripheryContract = useV1PeripheryContract()
+  const inputs = useMemo(
+    () => (marketAddresses ? marketAddresses.map(marketAddress => [marketAddress]) : []),
+    [marketAddresses],
+  )
+  const results = useSingleContractMultipleData(peripheryContract, 'marketState', inputs)
+
+  const loading = useMemo(() => results.some(({loading}) => loading), [results])
+  const error = useMemo(() => results.some(({error}) => error), [results])
+
+  const markets = useMemo(() => {
+    if (!loading && !error && marketAddresses) {
+      return results.map((call, index) => {
+        const marketAddress = marketAddresses[index]
+        const result = call.result as Result
+
+        return {
+          marketAddress,
+          bid: result.bid,
+          ask: result.ask,
+          mid: result.mid,
+          volumeBid: result.volumeBid,
+          voluemAsk: result.volumeAsk,
+          oiLong: result.oiLong,
+          oiShort: result.oiShort,
+          capOi: result.capOi,
+          circuitBreakerLevel: result.circuitBreakerLevel,
+          fundingRate: result.fundingRate,
+        }
+      })
+    }
+    return undefined
+  }, [loading, error, results, marketAddresses])
+
+  return {
+    loading,
+    error,
+    markets: markets?.map((market, index) => ({...market, marketAddress: inputs[index][0]})),
+  }
+}
