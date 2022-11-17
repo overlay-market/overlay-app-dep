99ee56d3eae427fd06f0f5e495126b6b754f767e
Jonah Lin <57741810+linjonah@users.noreply.github.com>
2022-11-17 09:42:02 -0800

    MarketStateDetails interface, to add to declaration type file later

--- src/hooks/useMarketState.ts
+++ src/hooks/useMarketState.ts
@@ -1,5 +1,19 @@
+import {BigNumber} from 'ethers'
 import {useV1PeripheryContract} from './useContract'
 import {useSingleContractMultipleData} from '../state/multicall/hooks'
 import {useBlockNumber} from '../state/application/hooks'
+
+interface MarketStateDetails {
+  bid: BigNumber
+  ask: BigNumber
+  mid: BigNumber
+  volumeBid: BigNumber
+  volumeAsk: BigNumber
+  oiLong: BigNumber
+  oiShort: BigNumber
+  capOi: BigNumber
+  circuitBreakerLevel: BigNumber
+  fundingRate: BigNumber
+}
 
 function useMarketStateFromAddresses() {}
