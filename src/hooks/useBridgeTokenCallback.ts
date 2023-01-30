ceaae4064cf69a8359af935cadd211669c0dac2b
Jonah Lin <57741810+linjonah@users.noreply.github.com>
2023-01-30 15:21:49 -0800

    useBridgeTokenCallbackProps

--- src/hooks/useBridgeTokenCallback.ts
+++ src/hooks/useBridgeTokenCallback.ts
@@ -2,8 +2,13 @@ import {useLayerZeroBridgeContract} from './useContract'
 import {useSingleContractMultipleData, useSingleCallResult} from '../state/multicall/hooks'
 import {useActiveWeb3React} from './web3'
 
+interface useBridgeTokenCallbackProps {
+  destinationChainId: number
+  amount: any
+}
+
 /**
  * Returns callback function that will execute bridging tokens from one chain to another
  * @param
  */
-export function useBridgeTokenCallback() {}
+export function useBridgeTokenCallback({destinationChainId, amount}: useBridgeTokenCallbackProps) {}
