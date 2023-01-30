742bfb2ccc28646998ab0bf16b12eefc982f3ac5
Jonah Lin <57741810+linjonah@users.noreply.github.com>
2023-01-30 15:20:18 -0800

    init useBridgeTokenCallback() hook

---
+++ src/hooks/useBridgeTokenCallback.ts
@@ -0,0 +1,9 @@
+import {useLayerZeroBridgeContract} from './useContract'
+import {useSingleContractMultipleData, useSingleCallResult} from '../state/multicall/hooks'
+import {useActiveWeb3React} from './web3'
+
+/**
+ * Returns callback function that will execute bridging tokens from one chain to another
+ * @param
+ */
+export function useBridgeTokenCallback() {}
