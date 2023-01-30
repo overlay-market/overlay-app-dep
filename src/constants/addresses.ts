a2c0a6665cf553df04e24ed642d78bed8b159464
Jonah Lin <57741810+linjonah@users.noreply.github.com>
2023-01-30 15:01:16 -0800

    Layer Zero contract address constants

--- src/constants/addresses.ts
+++ src/constants/addresses.ts
@@ -24,6 +24,12 @@ export const OVL_TOKEN_ADDRESS: AddressMap = {
   [ChainId.GÖRLI]: '0xdBD4a09ac1962F028390C53F4a4d126F5E13baEe',
 }
 
+// @dev: check addresses below to ensure they arent testing addresses
+export const LAYER_ZERO_ADDRESS: AddressMap = {
+  [ChainId.MAINNET]: '0x72e0Ab15AA457B1d2639f9411eA395A94FA57dfa', //testing
+  [ChainId.ARBITRUM]: '0x54dE374CE5e07C2F5a469f9bABA60D40eA142357', //testing
+}
+
 export const MULTICALL2_ADDRESS: AddressMap = {
   [ChainId.MAINNET]: '0x5BA1e12693Dc8F9c48aAD8770482f4739bEeD696',
   [ChainId.ROPSTEN]: '0x5BA1e12693Dc8F9c48aAD8770482f4739bEeD696',
