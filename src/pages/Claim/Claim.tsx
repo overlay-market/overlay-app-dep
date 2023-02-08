32471b79e9dda75ca33733aa2fbfd275979890cf
Jonah Lin <57741810+linjonah@users.noreply.github.com>
2023-02-07 17:28:03 -0800

    init claim slice in state

--- src/pages/Claim/Claim.tsx
+++ src/pages/Claim/Claim.tsx
@@ -1,5 +1,33 @@
+import styled from 'styled-components'
+import {TEXT} from '../../theme/theme'
+
+const BridgeContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin: auto;
+  max-width: 400px;
+  margin-top: 64px;
+  color: white;
+`
+
+const ClaimModal = styled.div`
+  display: flex;
+  flex-direction: column;
+  background: #1b2131;
+  border: 1px solid #71ceff;
+  box-shadow: 0 0 12px #5b60a4;
+  padding: 16px;
+  border-radius: 8px;
+`
+
 const Claim = () => {
-  return <>Claim</>
+  return (
+    <BridgeContainer>
+      <ClaimModal>
+        <TEXT.SmallBody>Claim OVL</TEXT.SmallBody>
+      </ClaimModal>
+    </BridgeContainer>
+  )
 }
 
 export default Claim
---
+++ src/state/claim/hooks.ts
@@ -0,0 +1,13 @@
+import {TransactionType} from './../transactions/actions'
+import {useState, useEffect} from 'react'
+import {TransactionResponse} from '@ethersproject/providers'
+import {CurrencyAmount, Token} from '@uniswap/sdk-core'
+import {useActiveWeb3React} from '../../hooks/web3'
+import JSBI from 'jsbi'
+import {useSingleCallResult} from '../multicall/hooks'
+import {isAddress} from '../../utils/web3'
+import {useContract} from '../../hooks/useContract'
+import {calculateGasMargin} from '../../utils/calculateGasMargin'
+import {useTransactionAdder} from '../transactions/hooks'
+
+export function useClaimCallback() {}
