b4a98a7746e0895fe0ca7b5093dba053973f9308
Jonah Lin <57741810+linjonah@users.noreply.github.com>
2023-02-07 17:51:50 -0800

    build children components in ClaimModal

--- src/pages/Claim/Claim.tsx
+++ src/pages/Claim/Claim.tsx
@@ -3,6 +3,8 @@ import {TEXT} from '../../theme/theme'
 import {useActiveWeb3React} from '../../hooks/web3'
 import {FlexRow, FlexColumn} from '../../components/Container/Container'
 import {shortenAddress} from '../../utils/web3'
+import {ExternalLink} from '../../components/ExternalLink/ExternalLink'
+import {TriggerActionButton} from '../../components/Button/Button'
 
 const BridgeContainer = styled.div`
   display: flex;
@@ -19,7 +21,6 @@ const ClaimModalContainer = styled.div`
   background: #1b2131;
   border: 1px solid #71ceff;
   box-shadow: 0 0 12px #5b60a4;
-  padding: 16px;
   border-radius: 8px;
 `
 
@@ -27,11 +28,19 @@ const ClaimModal = () => {
   const {account, chainId, error} = useActiveWeb3React()
   return (
     <ClaimModalContainer>
-      <FlexColumn padding="16px" border="">
-        <FlexRow>
+      <FlexColumn borderBottom="1px solid #71CEFF">
+        <FlexRow padding="16px">
           <TEXT.SmallBody>Claim OVL</TEXT.SmallBody>
           <TEXT.SmallBody>{account ? shortenAddress(account) : 'Not connected'}</TEXT.SmallBody>
         </FlexRow>
+      </FlexColumn>
+      <FlexColumn>
+        <TEXT.SmallBody>
+          As a member of the Overlay community, you may claim OVL to be used for voting and
+          governance, and to interact with the Overlay protocol.
+        </TEXT.SmallBody>
+        <ExternalLink href="">Read more about OVL</ExternalLink>
+        <TriggerActionButton isDisabled={true}>Claim OVL</TriggerActionButton>
       </FlexColumn>
     </ClaimModalContainer>
   )
