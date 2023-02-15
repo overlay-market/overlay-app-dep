82dfc1ecb09b6b97e4dbd5858e000d615ef43168
Jonah Lin <57741810+linjonah@users.noreply.github.com>
2023-02-15 09:07:38 -0800

    Add BAYC parsed market name

--- src/constants/markets.ts
+++ src/constants/markets.ts
@@ -2,4 +2,5 @@ export type MarketNameMap = {[marketId: string]: string}
 
 export const MARKET_NAME: MarketNameMap = {
   'Azuki Floor Price': 'Azuki / WETH',
+  'BAYC Floor Price': 'BAYC / WETH',
 }
