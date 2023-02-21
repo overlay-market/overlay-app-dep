020a56864b7d88af9436ac371a1c3c55e548b533
Jonah Lin <57741810+linjonah@users.noreply.github.com>
2023-02-20 17:46:08 -0800

    Init ClaimDataProps within ClaimPage

---
+++ src/pages/Claim/ClaimPage.tsx
@@ -0,0 +1,15 @@
+import styled from 'styled-components'
+
+interface ClaimDataProps {
+  platform: string
+  seatsAvailable: number
+  logoSrc: string
+  startDate: string
+  endDate: string
+}
+
+const ClaimPage = () => {
+  return <>Claim Page</>
+}
+
+export default ClaimPage
