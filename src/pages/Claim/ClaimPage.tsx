5285ecd4ddca7bee4d61a701fa619238b9adce14
Jonah Lin <57741810+linjonah@users.noreply.github.com>
2023-02-20 18:13:02 -0800

    Add ClaimData array to map through to render on Claim Page

--- src/pages/Claim/ClaimPage.tsx
+++ src/pages/Claim/ClaimPage.tsx
@@ -9,6 +9,88 @@ interface ClaimDataProps {
   endDate: string
 }
 
+const ClaimData: Array<ClaimDataProps> = [
+  {
+    platform: 'Beacon holders',
+    seatsAvailable: 222,
+    claimLink: '',
+    logoSrc: '',
+    startDate: '3/1',
+    endDate: '3/4',
+  },
+  {
+    platform: 'Overlay users',
+    seatsAvailable: 222,
+    claimLink: '',
+    logoSrc: '',
+    startDate: '3/2',
+    endDate: '3/5',
+  },
+  {
+    platform: 'Gearbox users',
+    seatsAvailable: 222,
+    claimLink: '',
+    logoSrc: '',
+    startDate: '3/6',
+    endDate: '3/7',
+  },
+  {
+    platform: 'Sense Finance users',
+    seatsAvailable: 222,
+    claimLink: '',
+    logoSrc: '',
+    startDate: '3/7',
+    endDate: '3/8',
+  },
+  {
+    platform: 'NFTPerp users',
+    seatsAvailable: 222,
+    claimLink: '',
+    logoSrc: '',
+    startDate: '3/8',
+    endDate: '3/9',
+  },
+  {
+    platform: 'Ambire Wallet users',
+    seatsAvailable: 222,
+    claimLink: '',
+    logoSrc: '',
+    startDate: '3/9',
+    endDate: '3/10',
+  },
+  {
+    platform: 'Element Finance users',
+    seatsAvailable: 222,
+    claimLink: '',
+    logoSrc: '',
+    startDate: '3/13',
+    endDate: '3/14',
+  },
+  {
+    platform: 'GMX users',
+    seatsAvailable: 222,
+    claimLink: '',
+    logoSrc: '',
+    startDate: '3/14',
+    endDate: '3/15',
+  },
+  {
+    platform: 'coW Swap users',
+    seatsAvailable: 222,
+    claimLink: '',
+    logoSrc: '',
+    startDate: '3/15',
+    endDate: '3/16',
+  },
+  {
+    platform: 'Arbitrum users',
+    seatsAvailable: 222,
+    claimLink: '',
+    logoSrc: '',
+    startDate: '3/16',
+    endDate: '3/17',
+  },
+]
 const Container = styled.div`
   display: flex;
   flex-wrap: wrap;
