7abc441f09e6928d606a4075009206df71950d48
Jonah Lin <57741810+linjonah@users.noreply.github.com>
2023-02-26 22:51:49 -0800

    Update Chainlink market names based on feed description

--- src/constants/markets.ts
+++ src/constants/markets.ts
@@ -3,4 +3,8 @@ export type MarketNameMap = {[marketId: string]: string}
 export const MARKET_NAME: MarketNameMap = {
   'Azuki Floor Price': 'Azuki / WETH',
   'BAYC Floor Price': 'BAYC / WETH',
+  'APE / USD': 'APE / USD',
+  'SOL / USD': 'SOL / USD',
+  'AVAX / USD': 'AVAX / USD',
+  'MATIC / USD': 'MATIC / USD',
 }
