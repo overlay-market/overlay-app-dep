6a4a901c1c1759fa234027a1e6f59f43f2e185e6
Jonah Lin <57741810+linjonah@users.noreply.github.com>
2023-02-27 13:56:34 -0800

    Add market name constants

--- src/constants/markets.ts
+++ src/constants/markets.ts
@@ -3,6 +3,8 @@ export type MarketNameMap = {[marketId: string]: string}
 export const MARKET_NAME: MarketNameMap = {
   'Azuki Floor Price': 'Azuki / WETH',
   'BAYC Floor Price': 'BAYC / WETH',
+  'LINK / USD': 'LINK / USD',
+  'WBTC / USD': 'WBTC / USD',
   'APE / USD': 'APE / USD',
   'SOL / USD': 'SOL / USD',
   'AVAX / USD': 'AVAX / USD',
