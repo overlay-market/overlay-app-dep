f8c8c0ef5ac0a5ff1e63036aa1312d4412514616
Jonah Lin <57741810+linjonah@users.noreply.github.com>
2023-02-28 17:16:49 -0800

    Add beacon-holders claim details

--- src/constants/claims.ts
+++ src/constants/claims.ts
@@ -1,20 +1,20 @@
 export declare enum ClaimId {
-  BEACON_HOLDERS = 1,
-  OVERLAY_USERS = 2,
-  GEARBOX_USERS = 3,
-  SENSE_FINANCE_USERS = 4,
-  NFT_PERP_USERS = 5,
-  AMBIRE_WALLET_USERS = 6,
-  ELEMENT_FINANCE_USERS = 7,
-  GMX_USERS = 8,
-  COW_SWAP_USERS = 9,
-  ARBITRUM_USERS = 10,
+  BEACON_HOLDERS = 'beacon-holders',
+  OVERLAY_USERS = 'overlay',
+  GEARBOX_USERS = 'gearbox',
+  SENSE_FINANCE_USERS = 'sense-finance',
+  NFT_PERP_USERS = 'nft-perp',
+  AMBIRE_WALLET_USERS = 'ambire-wallet',
+  ELEMENT_FINANCE_USERS = 'element-finance',
+  GMX_USERS = 'gmx',
+  COW_SWAP_USERS = 'cow-swap',
+  ARBITRUM_USERS = 'arbitrum',
 }
 
-export type ClaimMap = {[claimId: number]: string}
+export type ClaimMap = {[claimId: string]: string}
 
 export const MERKLE_DISTIBUTOR_ADDRESSES: ClaimMap = {
-  [ClaimId.BEACON_HOLDERS]: '',
+  [ClaimId.BEACON_HOLDERS]: '0xe00a6444686988C809bEa59bC8a18EA298a6A0a1',
   [ClaimId.OVERLAY_USERS]: '',
   [ClaimId.GEARBOX_USERS]: '',
   [ClaimId.SENSE_FINANCE_USERS]: '',
@@ -27,7 +27,8 @@ export const MERKLE_DISTIBUTOR_ADDRESSES: ClaimMap = {
 }
 
 export const MERKLE_PROOFS: ClaimMap = {
-  [ClaimId.BEACON_HOLDERS]: '',
+  [ClaimId.BEACON_HOLDERS]:
+    'https://raw.githubusercontent.com/overlay-market/MerkleDistributor/DegenScore/src/treeInfo.json',
   [ClaimId.OVERLAY_USERS]: '',
   [ClaimId.GEARBOX_USERS]: '',
   [ClaimId.SENSE_FINANCE_USERS]: '',
