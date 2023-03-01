c32c82250c272889e1161b32aa71b21922645844
Jonah Lin <57741810+linjonah@users.noreply.github.com>
2023-02-28 18:30:38 -0800

    Add new TransactionType to updater renders

--- src/state/transactions/updater.tsx
+++ src/state/transactions/updater.tsx
@@ -1,12 +1,11 @@
-import { ChainId } from '@sushiswap/sdk';
-import { useActiveWeb3React } from '../../hooks/web3';
-import { useCallback, useEffect, useMemo } from 'react';
-import { useAppDispatch, useAppSelector } from '../hooks';
-import { updateBlockNumber } from '../application/actions';
-import { useAddPopup, useBlockNumber } from '../application/hooks';
-import { RetryOptions, RetryableError, retry } from '../../utils/retry';
-import { checkedTransaction, finalizeTransaction, TransactionType } from './actions';
-
+import {ChainId} from '@sushiswap/sdk'
+import {useActiveWeb3React} from '../../hooks/web3'
+import {useCallback, useEffect, useMemo} from 'react'
+import {useAppDispatch, useAppSelector} from '../hooks'
+import {updateBlockNumber} from '../application/actions'
+import {useAddPopup, useBlockNumber} from '../application/hooks'
+import {RetryOptions, RetryableError, retry} from '../../utils/retry'
+import {checkedTransaction, finalizeTransaction, TransactionType} from './actions'
 
 interface TxInterface {
   addedTime: number
@@ -32,19 +31,19 @@ export function shouldCheck(lastBlockNumber: number, tx: TxInterface): boolean {
   }
 }
 
-const RETRY_OPTIONS_BY_CHAIN_ID: { [chainId: number]: RetryOptions } = {
-  [ChainId.HARMONY]: { n: 10, minWait: 250, maxWait: 1000 },
-  [ChainId.ARBITRUM]: { n: 10, minWait: 250, maxWait: 1000 },
+const RETRY_OPTIONS_BY_CHAIN_ID: {[chainId: number]: RetryOptions} = {
+  [ChainId.HARMONY]: {n: 10, minWait: 250, maxWait: 1000},
+  [ChainId.ARBITRUM]: {n: 10, minWait: 250, maxWait: 1000},
 }
-const DEFAULT_RETRY_OPTIONS: RetryOptions = { n: 3, minWait: 1000, maxWait: 3000 }
+const DEFAULT_RETRY_OPTIONS: RetryOptions = {n: 3, minWait: 1000, maxWait: 3000}
 
 export default function Updater(): null {
-  const { chainId, library } = useActiveWeb3React()
+  const {chainId, library} = useActiveWeb3React()
 
   const lastBlockNumber = useBlockNumber()
 
   const dispatch = useAppDispatch()
-  const state = useAppSelector((state) => state.transactions)
+  const state = useAppSelector(state => state.transactions)
 
   const transactions = useMemo(() => (chainId ? state[chainId] ?? {} : {}), [chainId, state])
 
@@ -57,31 +56,30 @@ export default function Updater(): null {
       const retryOptions = RETRY_OPTIONS_BY_CHAIN_ID[chainId] ?? DEFAULT_RETRY_OPTIONS
       return retry(
         () =>
-          library.getTransactionReceipt(hash).then((receipt) => {
+          library.getTransactionReceipt(hash).then(receipt => {
             if (receipt === null) {
               console.debug('Retrying for hash', hash)
               throw new RetryableError()
             }
             return receipt
           }),
-        retryOptions
+        retryOptions,
       )
     },
-    [chainId, library]
+    [chainId, library],
   )
 
   useEffect(() => {
-    
     if (!chainId || !library || !lastBlockNumber) return
 
     const cancels = Object.keys(transactions)
-      .filter((hash) => shouldCheck(lastBlockNumber, transactions[hash]))
-      .map((hash) => {
-        const { promise, cancel } = getReceipt(hash)
-        let transactionInfo = transactions[hash].info;
+      .filter(hash => shouldCheck(lastBlockNumber, transactions[hash]))
+      .map(hash => {
+        const {promise, cancel} = getReceipt(hash)
+        let transactionInfo = transactions[hash].info
 
         promise
-          .then((receipt) => {
+          .then(receipt => {
             if (receipt) {
               dispatch(
                 finalizeTransaction({
@@ -97,10 +95,10 @@ export default function Updater(): null {
                     transactionHash: receipt.transactionHash,
                     transactionIndex: receipt.transactionIndex,
                   },
-                })
+                }),
               )
 
-              console.log('receipt: ', receipt);
+              console.log('receipt: ', receipt)
 
               if (transactionInfo.type === TransactionType.APPROVAL) {
                 addPopup(
@@ -108,10 +106,10 @@ export default function Updater(): null {
                     txn: {
                       hash,
                       success: receipt.status === 1,
-                      info: transactionInfo
+                      info: transactionInfo,
                     },
                   },
-                  hash
+                  hash,
                 )
               }
 
@@ -121,10 +119,10 @@ export default function Updater(): null {
                     txn: {
                       hash,
                       success: receipt.status === 1,
-                      info: transactionInfo
+                      info: transactionInfo,
                     },
                   },
-                  hash
+                  hash,
                 )
               }
 
@@ -134,10 +132,10 @@ export default function Updater(): null {
                     txn: {
                       hash,
                       success: receipt.status === 1,
-                      info: transactionInfo
+                      info: transactionInfo,
                     },
                   },
-                  hash
+                  hash,
                 )
               }
 
@@ -147,23 +145,48 @@ export default function Updater(): null {
                     txn: {
                       hash,
                       success: receipt.status === 1,
-                      info: transactionInfo
+                      info: transactionInfo,
                     },
                   },
-                  hash
+                  hash,
                 )
               }
 
+              if (transactionInfo.type === TransactionType.BRIDGE_OVL) {
+                addPopup(
+                  {
+                    txn: {
+                      hash,
+                      success: receipt.status === 1,
+                      info: transactionInfo,
+                    },
+                  },
+                  hash,
+                )
+              }
+
+              if (transactionInfo.type === TransactionType.CLAIM_OVL) {
+                addPopup(
+                  {
+                    txn: {
+                      hash,
+                      success: receipt.status === 1,
+                      info: transactionInfo,
+                    },
+                  },
+                  hash,
+                )
+              }
 
               // the receipt was fetched before the block, fast forward to that block to trigger balance updates
               if (receipt.blockNumber > lastBlockNumber) {
-                dispatch(updateBlockNumber({ chainId, blockNumber: receipt.blockNumber }))
+                dispatch(updateBlockNumber({chainId, blockNumber: receipt.blockNumber}))
               }
             } else {
-              dispatch(checkedTransaction({ chainId, hash, blockNumber: lastBlockNumber }))
+              dispatch(checkedTransaction({chainId, hash, blockNumber: lastBlockNumber}))
             }
           })
-          .catch((error) => {
+          .catch(error => {
             // if (!error.isCancelledError) {
             //   console.error(`Failed to check transaction hash: ${hash}`, error)
             // }
@@ -172,7 +195,7 @@ export default function Updater(): null {
       })
 
     return () => {
-      cancels.forEach((cancel) => cancel())
+      cancels.forEach(cancel => cancel())
     }
   }, [chainId, library, transactions, lastBlockNumber, dispatch, addPopup, getReceipt])
 
