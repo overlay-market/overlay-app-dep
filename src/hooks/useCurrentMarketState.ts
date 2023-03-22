07ab5662faf1c823be3593d23cc413a7ce7b9bb9
Jonah Lin <57741810+linjonah@users.noreply.github.com>
2023-03-22 13:25:41 -0700

    useCurrentMarketState return type to include ParsedMarketStateDetails[]

--- src/hooks/useCurrentMarketState.ts
+++ src/hooks/useCurrentMarketState.ts
@@ -34,10 +34,10 @@ interface ParsedMarketStateDetails extends MarketStateDetails {
 interface UseMarketStateResults {
   loading: boolean
   error: boolean
-  markets: MarketStateDetails[] | undefined
+  markets: MarketStateDetails[] | ParsedMarketStateDetails[] | undefined
 }
 
-export function useCurrentMarketState(marketsData: AdditionalMarketData[] | undefined) {
+export function useCurrentMarketState(marketsData: AdditionalMarketData[] | undefined, sigFigs: number = 4): UseMarketStateResults {
   const peripheryContract = useV1PeripheryContract()
   const inputs = useMemo(() => (marketsData ? marketsData.map(market => [market.id]) : []), [marketsData])
   const results = useSingleContractMultipleData(peripheryContract, 'marketState', inputs)
@@ -47,8 +47,16 @@ export function useCurrentMarketState(marketsData: AdditionalMarketData[] | unde
   const markets = useMemo(() => {
     if (!loading && !error && marketsData) {
       return results.map((call, index) => {
-        const market = marketsData[index]
+        const market: AdditionalMarketData = marketsData[index]
+        const decimals = market.decimals
         const result = call.result as Result
+
+        const parsedBid = formatBigNumber(result.state_.bid, decimals, sigFigs)
+        const parsedAsk = formatBigNumber(result.state_.ask, decimals, sigFigs)
+        const parsedMid = formatBigNumber(result.state_.mid, decimals, sigFigs)
+        const parsedOiLong = formatBigNumber(result.state_.oiLong, decimals, sigFigs)
+        const parsedOiShort = formatBigNumber(result.state_.oiShort, decimals, sigFigs)
+        const parsedCapOi = formatBigNumber(result.state_.capOi, decimals, sigFigs)
 
         return {
           ...market,
@@ -62,11 +70,17 @@ export function useCurrentMarketState(marketsData: AdditionalMarketData[] | unde
           capOi: result.state_.capOi,
           circuitBreakerLevel: result.state_.circuitBreakerLevel,
           fundingRate: result.state_.fundingRate,
+          parsedBid,
+          parsedAsk,
+          parsedMid,
+          parsedOiLong,
+          parsedOiShort,
+          parsedCapOi,
         }
       })
     }
     return undefined
-  }, [loading, error, results, marketsData])
+  }, [loading, error, results, marketsData, sigFigs])
 
   return {
     loading,
