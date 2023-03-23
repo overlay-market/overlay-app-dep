4df2f5436cdb8304500c42ab89a4806412cb9b01
Jonah Lin <57741810+linjonah@users.noreply.github.com>
2023-03-23 12:20:29 -0700

    Render MarketsRow props in return statement

--- src/pages/Markets/MarketsRow.tsx
+++ src/pages/Markets/MarketsRow.tsx
@@ -1,14 +1,49 @@
+import {StyledTable, StyledHeaderCell, StyledTableCellThin, StyledTableRow, StyledTableHeaderRow} from '../../components/Table/Table'
+import {FlexRow} from '../../components/Container/Container'
+import {TEXT} from '../../theme/theme'
+import {DoubleProgressBar} from '../../components/ProgressBar/ProgressBar'
+
 type MarketsRowProps = {
   marketId: string
   marketName: string
   midPrice: string | number | undefined
   oiLong: string | number | undefined
   oiShort: string | number | undefined
+  totalCurrentOi: string | number | undefined
   capOi: string | number | undefined
   dailyFundingRate: string | number | undefined
   annualFundingRate: string | number | undefined
 }
 
-const MarketsRow = ({marketId, marketName, midPrice, oiLong, oiShort, capOi, dailyFundingRate, annualFundingRate}: MarketsRowProps) => {}
+const MarketsRow = ({
+  marketId,
+  marketName,
+  midPrice,
+  oiLong,
+  oiShort,
+  totalCurrentOi,
+  capOi,
+  dailyFundingRate,
+  annualFundingRate,
+}: MarketsRowProps) => {
+  return (
+    <StyledTableRow hover={true}>
+      <StyledTableCellThin component="th" scope="row">
+        {marketName}
+      </StyledTableCellThin>
+      <StyledTableCellThin align="left">{midPrice}</StyledTableCellThin>
+      <StyledTableCellThin align="left">
+        <FlexRow>
+          <TEXT.SmallBody mr="auto"></TEXT.SmallBody>
+          <TEXT.SmallBody></TEXT.SmallBody>
+        </FlexRow>
+        <DoubleProgressBar leftBarValue={Number(oiShort)} rightBarValue={Number(oiLong)} maxValue={Number(totalCurrentOi)} />
+      </StyledTableCellThin>
+      <StyledTableCellThin align="right">
+        {dailyFundingRate}% ({annualFundingRate}%)
+      </StyledTableCellThin>
+    </StyledTableRow>
+  )
+}
 
 export default MarketsRow
