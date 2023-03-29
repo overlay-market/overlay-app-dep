a321aa428f204a4d6f7f47dae9a6504db31a5207
Jonah Lin <57741810+linjonah@users.noreply.github.com>
2023-03-29 16:18:56 -0700

    Add constant to determine price currency type

--- src/constants/markets.ts
+++ src/constants/markets.ts
@@ -6,9 +6,6 @@ import SolanaLogo from '../assets/images/tokens/solana-logo.png'
 import ApeLogo from '../assets/images/tokens/ape-logo.png'
 
 export type DescriptionNameMap = {[description: string]: string}
-export type MarketNameAddressMap = {[marketAddress: string]: string}
-export type MarketLogoMap = {[marketBaseToken: string]: string}
-
 export const MARKET_NAME_FROM_DESCRIPTION: DescriptionNameMap = {
   'Azuki Floor Price': 'Azuki/ WETH',
   'BAYC Floor Price': 'BAYC / WETH',
@@ -22,10 +19,12 @@ export const MARKET_NAME_FROM_DESCRIPTION: DescriptionNameMap = {
   price: 'BAYC / WETH',
 }
 
+export type MarketNameAddressMap = {[marketAddress: string]: string}
 export const MARKET_NAME_FROM_ADDRESS: MarketNameAddressMap = {
   '0xb31d222c23104cbc2c04df77941f1f2c478133dd': 'BAYC / WETH',
 }
 
+export type MarketLogoMap = {[marketBaseToken: string]: string}
 export const MARKET_LOGO_FROM_BASE: MarketLogoMap = {
   WBTC: BitcoinLogo,
   BTC: BitcoinLogo,
@@ -36,4 +35,14 @@ export const MARKET_LOGO_FROM_BASE: MarketLogoMap = {
   SOL: SolanaLogo,
   APE: ApeLogo,
   BAYC: ApeLogo,
+}
+
+export type CurrencyMap = {[marketQuoteToken: string]: string}
+export const PRICE_CURRENCY_FROM_QUOTE: CurrencyMap = {
+  USD: '$',
+  USDC: '$',
+  DAI: '$',
+  ETH: 'Ξ',
+  WETH: 'Ξ',
+  AETH: 'Ξ',
 }
