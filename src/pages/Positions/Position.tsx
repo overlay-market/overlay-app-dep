15389549b9d6ab230ebfe9daaf5655096ae023cc
Jonah Lin <57741810+linjonah@users.noreply.github.com>
2023-04-06 15:08:53 -0700

    Describe PositionProps shape

--- src/pages/Positions/Position.tsx
+++ src/pages/Positions/Position.tsx
@@ -1,3 +1,14 @@
 import styled from 'styled-components'
 
-export const Position = () => {}
+export interface PositionProps {
+  id: string
+  marketName: string
+  marketAddress: string
+  createdTimestamp: string
+  isLong: boolean
+  entryPrice: string
+  liquidationPrice: string
+  currentMidPrice: string
+}
+
+export const Position = ({...props}: PositionProps) => {}
