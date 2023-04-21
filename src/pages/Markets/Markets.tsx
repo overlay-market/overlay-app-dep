4d83f3acc1030311aaebc272dd7316295e234099
governwith <dev@governwith.us>
2023-04-22 00:56:59 +0800

    fix console errors from map and muiTheme

--- src/pages/Markets/Markets.tsx
+++ src/pages/Markets/Markets.tsx
@@ -122,6 +122,7 @@ const Markets = () => {
                 ?.filter(market => !hiddenMarkets.includes(market.marketAddress.toLowerCase()))
                 .map((market: ParsedMarketStateDetails, index: number) => (
                   <MarketsRow
+                    key={market.marketAddress}
                     index={index + 1} //start count at 1
                     marketId={market.marketAddress}
                     marketName={market.marketName}
