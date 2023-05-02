28ca909824e247992a8563319b2bad1171806119
TomasCImach <tomas90@gmail.com>
2023-05-02 11:44:27 -0300

    change set isLong to true by default logic

--- src/pages/Markets/Build.tsx
+++ src/pages/Markets/Build.tsx
@@ -314,10 +314,6 @@ export const BuildInterface = ({marketId}: {marketId: string}) => {
     [onSelectPositionSide],
   )
 
-  if (isLong === undefined) {
-    handleSelectPositionSide(true)
-  }
-
   const handleUserInput = useCallback(
     (input: string) => {
       onAmountInput(input)
