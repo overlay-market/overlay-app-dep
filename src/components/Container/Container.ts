df1d8dab6e71e67dead005d61d86f2fe828a71ac
governwith <paulchristian.prieto98@gmail.com>
2023-06-06 02:01:34 +0800

    fix container margin on mobile

--- src/components/Container/Container.ts
+++ src/components/Container/Container.ts
@@ -4,14 +4,15 @@ import {Box} from 'rebass/styled-components'
 export const PageContainer = styled.div<{maxWidth?: string}>`
   max-width: ${({maxWidth}) => (maxWidth ? maxWidth : '1200px')};
   margin: auto;
-  margin-top: 48px;
+  margin-top: 0;
+  margin-bottom: 48px;
   padding: 16px;
 
   > div {
     background: ${({theme}) => theme.dark.background} !important;
   }
 
-  ${({theme}) => theme.mediaWidth.minMedium`
+  ${({theme}) => theme.mediaWidth.minSmall`
     margin: 0 auto 48px;
   `}
 `
